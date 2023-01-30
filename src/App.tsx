1b72389213572f0bea466eabf5f23a2a38c97421
Amdrii <andreyk557@gmail.com>
2023-01-30 19:03:54 +0200

    feat[app/comp]

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import {HomePage} from './pages/HomePage'
+import {FavouritesPage} from './pages/FavouritesPage'
+import {Navigation} from './components/Navigation'
 
 function App() {
   return (
-    <h1>hell</h1>
-  );
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={ <HomePage /> } />
+        <Route path="/favourites" element={ <FavouritesPage /> } />
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
